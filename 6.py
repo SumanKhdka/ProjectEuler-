5b1a1bc94e43774bd0aaf640037497f15bfcfcf9
Suman Khadka <sumankhadka00@gmail.com>
2024-04-09 11:16:06 +0545

    update

---
+++ 6.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    square_of_sum = (n * (n + 1) // 2) ** 2
+    sum_of_squares = n * (n + 1) * (2 * n + 1) // 6
+    print(square_of_sum - sum_of_squares)
