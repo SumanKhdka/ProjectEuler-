61f5ca56e2dc379793d6cdcc1cc8ee7750db0d36
Suman Khadka <sumankhadka00@gmail.com>
2024-04-09 11:14:59 +0545

    update

---
+++ 5.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+
+def gcd(a, b):
+    if b:
+        return gcd(b, a % b)
+    else:
+        return a
+
+t = int(input())
+for _ in range(t):
+    product = 1
+    n = int(input())
+    for number in range(2, n + 1):
+        product *= number // gcd(product, number)
+    print(product)
