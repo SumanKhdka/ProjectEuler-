e88326ec7f6a87146cda05c8e288a85cc3a06213
Suman Khadka <sumankhadka00@gmail.com>
2024-04-11 10:59:32 +0545

    update

--- 21.py
+++ 21.py
@@ -1,2 +1,29 @@
 #21: Amicable numbers
 
+def sum_of_divisors(n):
+    sum = 1
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            if i * (n // i) == n:
+                sum = sum + i + n//i
+            else:
+                sum = sum + i
+            i += 1
+    return sum
+
+def find_amicable_numbers(N):
+    amicable_sum = 0
+    for a in range(1, N):
+        b = sum_of_divisors(a)
+        if a != b and sum_of_divisors(b) == a:
+            amicable_sum += a
+    return amicable_sum
+
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    print(find_amicable_numbers(N))
---
+++ 73.py
@@ -0,0 +1,76 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+#!/bin/python3
+
+import sys
+import math
+import time
+
+
+
+def fac(n):
+    res = 0
+    for num in str(n):
+        res += FACTORIAL10[int(num)]
+    return res
+
+FACTORIAL10 = [math.factorial(i) for i in range(10)]
+
+mem = {}
+def get_chain(n):
+    chain = []
+    while True:
+        chain.append(n)
+        if n in mem:
+            n = mem[n]
+        else:
+            i = fac(n)
+            mem[n] = i
+            n = i
+        if n in chain:
+            break
+    return chain
+
+# circular chains up to 10^6
+circular_chains = {
+    #[145, 145]
+    145: 1,
+    #[169, 363601, 1454, 169]
+    169: 3,
+    #[871, 45361, 871]
+    871: 2,
+    #[872, 45362, 872]
+    872: 2,
+    #[1454, 169, 363601, 1454]
+    1454: 3,
+    #[40585, 40585]
+    40585: 1,
+    #[45361, 871, 45361]
+    45361: 2,
+    #[45362, 872, 45362]
+    45362: 2,
+    #[363601, 1454, 169, 363601]
+    363601: 3,
+}
+
+inputs = [tuple(map(int, input().strip().split())) for _ in range(int(input().strip()))]
+
+st = time.time()
+
+seen = {}
+for N, L in inputs:
+    res = []
+    for i in range(N + 1):
+        chain = 0
+        if i in circular_chains:
+            chain = circular_chains[i]
+        else:
+            s = "".join(sorted(str(i)))
+            chain = seen[s] if s in seen else len(get_chain(i))
+            seen[s] = chain
+        if chain == L:
+            res.append(i)
+    if len(res) > 0:
+        print(" ".join(map(str, res)))
+    else:
+        print(-1)
